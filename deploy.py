9a53135c4696a80465f7226b126a86dabc86cc6a
Jonasz Aleszkiewicz <43296251+Aleshkev@users.noreply.github.com>
2023-01-20 02:41:16 +0100

    preserve archival content

--- deploy.py
+++ deploy.py
@@ -1,4 +1,3 @@
 from os import system
 
-system("python ./scripts/build_wrapper.py")
 system("push-dir --dir=a --branch=gh-pages --cleanup --verbose")
--- script/build_wrapper.py
+++ script/build_wrapper.py
@@ -7,3 +7,7 @@ from pathlib import Path
 
 for p in Path("a/").glob("*.html"):
     p.write_text(p.read_text("utf-8").replace("/a/", "/v-1.5/").replace("/a\"", "/v-1.5\""), "utf-8")
+
+from os import system
+
+system("cp -r content/ a/content/")
