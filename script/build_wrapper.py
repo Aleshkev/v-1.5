aca72eaf89a90d7297b078cf775ee89117491418
Jonasz Aleszkiewicz <43296251+Aleshkev@users.noreply.github.com>
2023-01-20 02:42:37 +0100

    fix the link

--- script/build_wrapper.py
+++ script/build_wrapper.py
@@ -6,8 +6,8 @@ build.Site(None, True, False)
 from pathlib import Path
 
 for p in Path("a/").glob("*.html"):
-    p.write_text(p.read_text("utf-8").replace("/a/", "/v-1.5/").replace("/a\"", "/v-1.5\""), "utf-8")
+    p.write_text(p.read_text("utf-8").replace("/a/", "/v-1.5/").replace("/a\"", "/v-1.5\"").replace("/content/", "/v-1/"), "utf-8")
 
 from os import system
 
-system("cp -r content/ a/content/")
+system("cp -r content/ a/archive/")
